cecaa3526b224e9b77c9386f017ac7e6341772ea
Mohammad Iqbal Ferdiansyah <53143074+MohIqbalF@users.noreply.github.com>
2019-08-01 20:16:06 +0700

    Add files via upload

---
+++ MyIntentApp/MyIntentApp/app/src/main/java/com/example/myintentapp/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.myintentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnMoveActivity = findViewById(R.id.btn_move_activity);
+        btnMoveActivity.setOnClickListener(this);
+
+        Button btnMoveWithDataActivity = findViewById(R.id.btn_move_activity_data);
+        btnMoveWithDataActivity.setOnClickListener(this);
+
+        Button btnDialPhone = findViewById(R.id.btn_dial_number);
+        btnDialPhone.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+
+            case R.id.btn_move_activity:
+                Intent moveIntent = new Intent(MainActivity.this, MoveActivity.class);
+                startActivity(moveIntent);
+                break;
+
+            case R.id.btn_move_activity_data:
+                Intent moveWithDataIntent = new Intent(MainActivity.this, MoveWithDataActivity.class);
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "DicodingAcademyBoy");
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 5);
+                startActivity(moveWithDataIntent);
+                break;
+
+            case R.id.btn_dial_number:
+                String phoneNumber = "081210841382";
+                Intent dialPhoneIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+phoneNumber));
+                startActivity(dialPhoneIntent);
+                break;
+        }
+    }
+}
